1d62b4bffdc997c4e4349c5bcc0f7a655b991d45
Ed Porras <dev@digressed.net>
2019-05-22 21:54:38 +0200

    Add descriptors and streams to BAT test. Remove traces.

--- tests/test_table_gen.cc
+++ tests/test_table_gen.cc
@@ -45,7 +45,6 @@ namespace data
       inf.seekg(0, std::ios::beg);
       inf.read(blob, size);
       inf.close();
-      std::cerr << "read " << size << " bytes, data: " << blob << std::endl;
 
       // compare
       std::ostringstream os;
@@ -53,7 +52,6 @@ namespace data
                     [&](const Section* section) {
                        section->write(os);
                     } );
-      std::cerr << "os size: " << os.str().length() << ", data: " << os.str() << std::endl;
       int cmp = std::memcmp(os.str().c_str(), blob, size);
 
       delete [] blob;
@@ -357,6 +355,13 @@ int main(int argc, char* argv[])
    if (flags & BAT_F)
    {
       BAT bat(0x1000, 0x10);
+
+      bat.addXportStream(100, 200);
+      bat.addXportStream(100, 201);
+      BouquetNameDesc *bnd = new BouquetNameDesc("Bouquet Name");
+      bat.addBouquetDesc(*bnd);
+      StreamIdentifierDesc *sid = new StreamIdentifierDesc(0x02);
+      bat.addXportStreamDesc(100, 201, *sid);
 
       DUMP(bat);
       bat.buildSections(t);
