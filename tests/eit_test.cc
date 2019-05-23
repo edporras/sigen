25bc3d587b829062d1eb04a4c400abc840b919ce
Ed Porras <dev@digressed.net>
2019-05-23 21:06:51 +0200

    Better tests for EIT.

--- tests/eit_test.cc
+++ tests/eit_test.cc
@@ -7,11 +7,33 @@ namespace tests
 {
    int eit(TStream& t)
    {
-      // TODO: this needs more descriptors, events, etc.
-      PF_EIT eit(100, 0x333, 0x444, PF_EIT::ACTUAL, 0);
+      // EIT PF Actual
+      PF_EITActual eit_a(100, 0x333, 0x444, 0);
 
-      DUMP(eit);
-      eit.buildSections(t);
+      eit_a.addPresentEvent(0x1000, UTC(3, 1, 1999, 9, 0, 0), BCDTime(0, 30, 0), 1, 1);
+      eit_a.addFollowingEvent(0x1001, UTC(3, 1, 1999, 9, 30, 0), BCDTime(0, 30, 0), 1, 1);
+
+      PDCDesc* pdcd = new PDCDesc(0x1000);
+      eit_a.addPresentEventDesc(*pdcd);
+
+      DSNGDesc* dsngd = new DSNGDesc("dsng data");
+      eit_a.addFollowingEventDesc(0x1001, *dsngd);
+
+      PartialTransportStreamDesc* ptsd = new PartialTransportStreamDesc(0x3000, 0x50, 0x1000);
+      eit_a.addFollowingEventDesc(0x1001, *ptsd);
+
+      TransportStreamDesc* tsd = new TransportStreamDesc;
+      eit_a.addFollowingEventDesc(0x1001, *tsd);
+
+      DUMP(eit_a);
+      eit_a.buildSections(t);
+
+      // EIT PDF Other
+      PF_EITOther eit_o(101, 0x444, 0x555, 0);
+      eit_o.addPresentEvent(0x2000, UTC(3, 1, 1999, 9, 0, 0), BCDTime(0, 30, 0), 1, 1);
+
+      DUMP(eit_o);
+      eit_o.buildSections(t);
 
       return tests::cmp_bin(t, "reference/eit.ts");
    }
