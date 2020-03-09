6ecf5cbb65695a941d061b501fb8edf1a5096bdf
Ed Porras <dev@digressed.net>
2020-03-09 08:32:06 +0100

    set flycheck to use c++-14

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((c++-mode
-  (flycheck-clang-language-standard . "c++11")
-  (flycheck-gcc-language-standard . "c++11")))
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-gcc-language-standard . "c++14")))
